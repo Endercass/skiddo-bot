725fb120bc15de3675789ccdf6c5bbe1e73ce8a8
endercass <46408156+Endercass@users.noreply.github.com>
2024-01-30 23:00:12 -0500

    FileSystem implementation

--- src/js_runtime/skiddo_instance/js/ext.js
+++ src/js_runtime/skiddo_instance/js/ext.js
@@ -1,7 +1,22 @@
 ((globalThis) => {
   const core = Deno.core;
+
+  const op_init_state = core.ops.op_init_state;
+
   const skiddo_file = core.ops.op_get_skiddo_file();
+  const op_fs_create_file = core.ops.op_fs_create_file;
+  const op_fs_read_file = core.ops.op_fs_read_file;
+  const op_fs_write_file = core.ops.op_fs_write_file;
+  const op_fs_remove_file = core.ops.op_fs_read_dir;
+  const op_fs_create_dir = core.ops.op_fs_create_dir;
+  const op_fs_read_dir = core.ops.op_fs_read_dir;
+  const op_fs_remove_dir = core.ops.op_fs_remove_dir;
+  const op_fs_stat = core.ops.op_fs_stat;
+  const op_fs_copy = core.ops.op_fs_copy;
+  const op_fs_rename = core.ops.op_fs_rename;
+
   const op_log = core.ops.op_log;
+
   function argsToMessage(...args) {
     const seen = new WeakSet();
     return args
@@ -32,6 +47,138 @@
       },
     },
   });
+
+  const rust_vec_to_buffer = (vec) => {
+    // Vec is object with as many keys as elements.
+    // The keys are string representations of numbers.
+    // The values are the elements.
+    const array = new Array(vec.length);
+    for (const key in vec) {
+      array[Number(key)] = vec[key];
+    }
+    return array;
+  };
+
+  // Polyfill TextEncoder
+  class TextEncoder {
+    constructor(encoding = "utf-8") {
+      if (encoding.toLocaleLowerCase() !== "utf-8") {
+        throw new Error(`Unsupported encoding: ${encoding}`);
+      }
+    }
+
+    encode(str) {
+      // Try our best to allocate a buffer that is big enough.
+      // UTF8 is a variable width encoding, however, so we may over-allocate.
+      let utf8Bytes = new Array(str.length * 4);
+      for (let i = 0; i < str.length; i++) {
+        let code = str.charCodeAt(i);
+        if (code < 128) {
+          utf8Bytes.push(code);
+        } else if (code < 2048) {
+          utf8Bytes.push((code >> 6) | 192);
+          utf8Bytes.push((code & 63) | 128);
+        } else if (code < 65536) {
+          utf8Bytes.push((code >> 12) | 224);
+          utf8Bytes.push(((code >> 6) & 63) | 128);
+          utf8Bytes.push((code & 63) | 128);
+        } else {
+          utf8Bytes.push((code >> 18) | 240);
+          utf8Bytes.push(((code >> 12) & 63) | 128);
+          utf8Bytes.push(((code >> 6) & 63) | 128);
+          utf8Bytes.push((code & 63) | 128);
+        }
+      }
+      // Trim off any over-allocation.
+      utf8Bytes = utf8Bytes.flat();
+      return new Uint8Array(utf8Bytes);
+    }
+  }
+
+  // Polyfill TextDecoder
+  class TextDecoder {
+    constructor(encoding = "utf-8") {
+      if (encoding.toLocaleLowerCase() !== "utf-8") {
+        throw new Error(`Unsupported encoding: ${encoding}`);
+      }
+    }
+
+    decode(data) {
+      let result = "";
+      let i = 0;
+
+      while (i < data.length) {
+        let byte = data[i];
+
+        if (byte < 128) {
+          result += String.fromCharCode(byte);
+          i++;
+        } else if (byte < 192) {
+          throw new Error("Invalid UTF-8 sequence");
+        } else if (byte < 224) {
+          result += String.fromCharCode(
+            ((byte & 31) << 6) | (data[i + 1] & 63)
+          );
+          i += 2;
+        } else if (byte < 240) {
+          result += String.fromCharCode(
+            ((byte & 15) << 12) | ((data[i + 1] & 63) << 6) | (data[i + 2] & 63)
+          );
+          i += 3;
+        } else {
+          result += String.fromCharCode(
+            ((byte & 7) << 18) |
+              ((data[i + 1] & 63) << 12) |
+              ((data[i + 2] & 63) << 6) |
+              (data[i + 3] & 63)
+          );
+          i += 4;
+        }
+      }
+
+      return result;
+    }
+  }
+
+  // Populate global context with the filesystem API
+  Object.defineProperty(globalThis, "fs", {
+    value: {
+      createFileSync: (path) => {
+        return op_fs_create_file(path);
+      },
+      readFileSync: (path) => {
+        return rust_vec_to_buffer(op_fs_read_file(path));
+      },
+      writeFileSync: (path, data) => {
+        if (typeof data === "string") {
+          data = new TextEncoder().encode(data);
+        }
+        return op_fs_write_file(path, data);
+      },
+      rmSync: (path) => {
+        return op_fs_remove_file(path);
+      },
+      createDirSync: (path) => {
+        return op_fs_create_dir(path);
+      },
+      readDirSync: (path) => {
+        return op_fs_read_dir(path).split("\n");
+      },
+      rmdirSync: (path) => {
+        return op_fs_remove_dir(path);
+      },
+      statSync: (path) => {
+        return op_fs_stat(path);
+      },
+      copySync: (from, to) => {
+        return op_fs_copy(from, to);
+      },
+      renameSync: (from, to) => {
+        return op_fs_rename(from, to);
+      },
+    },
+  });
+
   let timers = {
     setTimeout: function (callback, delay) {
       return core.queueTimer(core.getTimerDepth() + 1, false, delay, callback);
@@ -70,7 +217,41 @@
     configurable: false,
     enumerable: true,
   });
+
+  Object.defineProperty(globalThis, "TextEncoder", {
+    value: TextEncoder,
+    writable: false,
+    configurable: false,
+    enumerable: true,
+  });
+
+  Object.defineProperty(globalThis, "TextDecoder", {
+    value: TextDecoder,
+    writable: false,
+    configurable: false,
+    enumerable: true,
+  });
+
+  op_init_state();
+
+  globalThis.console.log("Hello world!");
+
   globalThis.console.log(globalThis.skiddo);
+
+  globalThis.console.log(globalThis.fs);
+
+  globalThis.console.log(
+    new TextDecoder().decode(globalThis.fs.readFileSync("/test.txt"))
+  );
+
+  globalThis.fs.writeFileSync(
+    "/test2.txt",
+    "Hello" + " " + "\u{1F496}".repeat(5) + " " + "world!"
+  );
+
+  globalThis.console.log(
+    new TextDecoder().decode(globalThis.fs.readFileSync("/test2.txt"))
+  );
 })(globalThis);
 
 // Sandbox :3
--- src/js_runtime/skiddo_instance/js/ext.rs
+++ src/js_runtime/skiddo_instance/js/ext.rs
@@ -1,7 +1,20 @@
+use std::io::Write;
+
+use deno_core::error::AnyError;
 use deno_core::extension;
 use deno_core::op2;
+use deno_core::OpState;
+use gotham::state::{State, StateData};
 use scorched::{logf, LogData, LogImportance};
 use serde::{Deserialize, Serialize};
+use vfs::impls::memory::MemoryFS;
+use vfs::FileSystem;
+use vfs::VfsMetadata;
+
+#[derive(StateData)]
+pub struct SkiddoState {
+    pub fs: MemoryFS,
+}
 
 #[derive(Serialize, Deserialize)]
 pub enum LogLevel {
@@ -21,6 +34,153 @@ fn op_log(#[serde] level: LogLevel, #[string] msg: String) {
     };
 }
 
+#[op2(fast)]
+fn op_init_state(state: &mut OpState) {
+    let skiddo_state = SkiddoState {
+        fs: MemoryFS::new(),
+    };
+    // Iniiialize the filesystem with test file
+    let mut file = skiddo_state.fs.create_file("/test.txt").unwrap();
+    file.write_all(b"Hello, world!").unwrap();
+
+    state.put(skiddo_state);
+}
+
+#[op2(fast)]
+fn op_fs_create_file(state: &mut OpState, #[string] path: String) -> Result<(), AnyError> {
+    let skiddo_state = state.borrow::<SkiddoState>();
+    skiddo_state.fs.create_file(&path)?;
+    Ok(())
+}
+
+#[op2]
+#[buffer]
+fn op_fs_read_file(state: &mut OpState, #[string] path: String) -> Result<Vec<u8>, AnyError> {
+    let skiddo_state = state.borrow::<SkiddoState>();
+    let mut file = skiddo_state.fs.open_file(&path)?;
+    let mut buf = Vec::new();
+    file.read_to_end(&mut buf)?;
+    Ok(buf)
+}
+
+#[op2(fast)]
+fn op_fs_write_file(
+    state: &mut OpState,
+    #[string] path: String,
+    #[buffer] buf: &[u8],
+) -> Result<(), AnyError> {
+    let skiddo_state = state.borrow::<SkiddoState>();
+    if skiddo_state.fs.exists(&path)? {
+        skiddo_state.fs.remove_file(&path)?;
+    }
+    let mut file = skiddo_state.fs.create_file(&path)?;
+    file.write_all(buf)?;
+    Ok(())
+}
+
+#[op2(fast)]
+fn op_fs_remove_file(state: &mut OpState, #[string] path: String) -> Result<(), AnyError> {
+    let skiddo_state = state.borrow::<SkiddoState>();
+    skiddo_state.fs.remove_file(&path)?;
+    Ok(())
+}
+
+#[op2(fast)]
+fn op_fs_create_dir(state: &mut OpState, #[string] path: String) -> Result<(), AnyError> {
+    let skiddo_state = state.borrow::<SkiddoState>();
+    skiddo_state.fs.create_dir(&path)?;
+    Ok(())
+}
+
+#[op2(fast)]
+fn op_fs_remove_dir(state: &mut OpState, #[string] path: String) -> Result<(), AnyError> {
+    let skiddo_state = state.borrow::<SkiddoState>();
+    skiddo_state.fs.remove_dir(&path)?;
+    Ok(())
+}
+
+#[derive(Serialize, Deserialize)]
+struct SkiddoMetadata {
+    is_dir: bool,
+    len: u64,
+}
+
+impl From<VfsMetadata> for SkiddoMetadata {
+    fn from(meta: VfsMetadata) -> Self {
+        Self {
+            is_dir: match meta.file_type {
+                vfs::path::VfsFileType::File => false,
+                vfs::path::VfsFileType::Directory => true,
+            },
+            len: meta.len,
+        }
+    }
+}
+
+#[op2]
+#[serde]
+fn op_fs_stat(state: &mut OpState, #[string] path: String) -> Result<SkiddoMetadata, AnyError> {
+    let skiddo_state = state.borrow::<SkiddoState>();
+    Ok(skiddo_state.fs.metadata(&path)?.into())
+}
+
+#[op2(fast)]
+fn op_fs_copy(
+    state: &mut OpState,
+    #[string] from: String,
+    #[string] to: String,
+) -> Result<(), AnyError> {
+    let skiddo_state = state.borrow::<SkiddoState>();
+    match skiddo_state.fs.metadata(&from)?.file_type {
+        vfs::path::VfsFileType::File => {
+            let mut from_file = skiddo_state.fs.open_file(&from)?;
+            let mut to_file = skiddo_state.fs.create_file(&to)?;
+            std::io::copy(&mut from_file, &mut to_file)?;
+        }
+        vfs::path::VfsFileType::Directory => {
+            // :( TODO
+            Err(std::io::Error::new(
+                std::io::ErrorKind::Other,
+                "Copying directories is not supported yet",
+            ))?
+        }
+    }
+    Ok(())
+}
+
+#[op2(fast)]
+fn op_fs_rename(
+    state: &mut OpState,
+    #[string] from: String,
+    #[string] to: String,
+) -> Result<(), AnyError> {
+    let skiddo_state = state.borrow::<SkiddoState>();
+    match skiddo_state.fs.metadata(&from)?.file_type {
+        vfs::path::VfsFileType::File => {
+            let mut from_file = skiddo_state.fs.open_file(&from)?;
+            let mut to_file = skiddo_state.fs.create_file(&to)?;
+            std::io::copy(&mut from_file, &mut to_file)?;
+            skiddo_state.fs.remove_file(&from)?;
+        }
+        vfs::path::VfsFileType::Directory => {
+            // :( TODO
+            Err(std::io::Error::new(
+                std::io::ErrorKind::Other,
+                "Renaming directories is not supported yet",
+            ))?
+        }
+    }
+    Ok(())
+}
+
+#[op2]
+#[string]
+fn op_fs_read_dir(state: &mut OpState, #[string] path: String) -> Result<String, AnyError> {
+    let skiddo_state = state.borrow::<SkiddoState>();
+    let entries: Vec<String> = skiddo_state.fs.read_dir(&path)?.collect();
+    Ok(entries.join("\n"))
+}
+
 #[op2]
 #[string]
 fn op_get_skiddo_file() -> Option<String> {
@@ -36,7 +196,7 @@ fn op_get_skiddo_file() -> Option<String> {
 
 extension!(
     skiddo_internal,
-    ops = [op_log, op_get_skiddo_file],
+    ops = [op_log, op_get_skiddo_file, op_init_state, op_fs_create_file, op_fs_read_file, op_fs_write_file, op_fs_remove_file, op_fs_create_dir, op_fs_read_dir, op_fs_remove_dir, op_fs_stat, op_fs_copy, op_fs_rename],
     esm_entry_point = "ext:skiddo_internal/ext.js",
     esm = [
         dir "src/js_runtime/skiddo_instance/js",
